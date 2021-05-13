bd65abf5d24671276037b22476c41c98c040b322
bharathtej <chinimillibharath@gmail.com>
2021-05-13 17:52:40 +0530

    second commit

---
+++ Java tutorial/src/arrays/ArrayDemo.java
@@ -0,0 +1,23 @@
+package arrays;
+
+public class ArrayDemo {
+
+	public static void main(String[] args) {
+		// Arrays are container object that holds fixed number of values of single type
+		
+		// Declare array of integers
+		
+		int [] anArray;
+		
+		// Allocate memory for 10 integers
+		
+		anArray = new int[10];
+		
+		// initialize all the ten elements
+		
+		for(int i=0; i<10;i++) {
+			anArray[i]=i*100+100;
+			System.out.println(" Element at index "+i+" is "+ anArray[i] );		
+			}
+	}
+}
